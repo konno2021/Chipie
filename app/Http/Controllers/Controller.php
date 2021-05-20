8d75a012fac5957119a191fcd897498b577071ab
konno2021 <2021php41-student009@la-study.com>
2021-05-20 14:24:45 +0900

    create header footer

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,22 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    static public function get_user_status(){
+        $user_status = -1;
+
+        if(Auth::check() === false){
+            $user_status = 0;  // 非会員
+        }
+        else if(Auth::user()->inn_id !== null){
+            $user_status = 2;  // 宿管理者
+        }
+        else if(Auth::user()->is_admin){
+            $user_status = 3;    // 管理者
+        }
+        else{
+            $user_status = 1;
+        }
+        return $user_status;
+    }
 }
