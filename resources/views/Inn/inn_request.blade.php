a61539ece0a0c84aaac6c8ed58cc3279220a6c67
kageyamasan2 <2021php41-student013@la-study.com>
2021-05-21 09:13:28 +0900

    5/21修正しました

--- resources/views/Inn/inn_request.blade.php
+++ resources/views/Inn/inn_request.blade.php
@@ -6,38 +6,29 @@
 
 
     <div class="col">
-            <label for="inputAddress"class="font-weight-bold">宿名</label>
-          <input type="text" name="name" class="form-control" placeholder="宿名" value="{{old('name')}}">
+            <label for="inputAddress"class="font-weight-bold  ml-3">宿名</label>
+          <input type="text" name="name" class="form-control  ml-3" placeholder="宿名" value="{{old('name')}}">
         
     </div>
 
     <br>
-    <p class="font-weight-bold col " >分類コード<p>
-    <div class="form-check form-check-inline ml-3">
-        <input class="form-check-input" type="radio" id="inlineCheckbox1" name="inn_code_id" value="{{old('inn_code_id')}}">
-        <label class="form-check-label" for="inlineCheckbox1">0</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" id="inlineCheckbox2" name="inn_code_id" value="{{old('inn_code_id')}}">
-        <label class="form-check-label" for="inlineCheckbox2">1</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" id="inlineCheckbox3" name="inn_code_id" value="{{old('inn_code_id')}}">
-        <label class="form-check-label" for="inlineCheckbox3">2</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" id="inlineCheckbox3" name="inn_code_id" value="{{old('inn_code_id')}}">
-        <label class="form-check-label" for="inlineCheckbox3">3</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" id="inlineCheckbox3" name="inn_code_id" value="{{old('inn_code_id')}}">
-        <label class="form-check-label" for="inlineCheckbox3">4</label>
-      </div>
-      <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" id="inlineCheckbox3" name="inn_code_id" value="{{old('inn_code_id')}}">
-        <label class="form-check-label" for="inlineCheckbox3">5</label>
-      </div>
-    <p></p>
+    <div class="col-5">
+        <dl>
+            <dd>
+                <dt  class=" ml-3">分類コード</dt>
+                    <select name="inn_code_id" size="1" class=" ml-3">
+                        <option value="0">シティホテル</option>
+                        <option value="1">リゾートホテル</option>
+                        <option value="2">ビジネスホテル</option>
+                        <option value="3">旅館</option>
+                        <option value="4">民宿</option> 
+                        <option value="5">ペンション</option>
+                    </select>
+            </dd>
+        </dl>
+        <dl>
+    <br>
+    
     <div class="form-group font-weight-bold col">
         <label for="inputAddress">住所</label>
         <input type="text" class="form-control" id="inputAddress" placeholder="東京都新宿区3丁目2-2" name="address" value="{{old('address')}}">
@@ -48,22 +39,24 @@
             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="chipie@index.com" name="email" value="{{old('email')}}">
             <small id="emailHelp" class="form-text text-muted">お間違えの無いようお願いします。</small>
     </div>
-    
+
+    <br>
 
     <div class="form-group font-weight-bold col">
         <label for="exampleInputPassword1">チェックイン時間：</label>
         <input type="time"  id="exampleInputPassword1" name="check_in" value="{{old('check_in')}}">
     </div>
-
+    
     <div class="form-group font-weight-bold col ">
         <label for="exampleInputPassword1">チェックアウト時間：</label>
         <input type="time"  id="exampleInputPassword1" name="check_out" value="{{old('check_out')}}">
+<br>
     </div>
      <div class="col">
         <label for="inputAddress"class="font-weight-bold">宿のHP</label>
       <input type="text" class="form-control" placeholder="https://" name="hp" value="{{old('hp')}}">
     </div> 
-
+<br>
     <div class="form-group font-weight-bold col">
         <label for="exampleInputPassword1">パスワード</label>
         <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password" value="{{old('password')}}">
@@ -74,8 +67,7 @@
                     <label class="form-check-label " for="exampleCheck1">ご確認の上クリックをお願いします</label>
      </div>
     
-     
-
      <button type="submit" class="btn btn-primary col">申請</button>
   </form>
+</div>
 @endsection
