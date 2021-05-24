99cccf18cdf011239087fb1b2e933603e27a0acb
Taneishi-minori <m.taneisi2021php@gmail.com>
2021-05-24 17:44:18 +0900

    taneisihi

--- resources/views/Inn/inn_show.blade.php
+++ resources/views/Inn/inn_show.blade.php
@@ -32,10 +32,9 @@
         @foreach ($plans as $plan)
             {{$plan->plan_name}}
             {{-- @foreach($plan->posts as $post)でいけるはず・・・？ --}}
-        @endforeach
+        
         {{-- <form action="{{ route('reservation.confirm') }}" method="post"> --}}
-
-        <button class="btn btn-primary" type="submit">このプランで予約</button></p>
+        <a href="{{ route('reservations.create', $plan) }}" class="btn btn-primary">このプランで予約</a></p>
 
         <p class="text-right">
             <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
@@ -44,15 +43,36 @@
         </p>
         <div class="collapse" id="collapseExample">
             <div class="card card-body text-right">
-                {{-- @foreach ($plans as $plan)
+                <div>
+                @foreach ($plan->posts as $post)
+                {{ $post->value }} 
+                {{ $post->title }}
+                {{ $post->poster_name }}
                 {{ $post->content }}
-                @endforeach --}}
-                <form action="{{ route('inns.show', $inn) }}" method="post">
-                    @csrf
-                    <label class="text-left">口コミ投稿<br>
-                    <input type="text" name="content" value="content"></label>
-                <button type="submit">投稿</button>
+                @endforeach
+                </div>
+                <div>
+                    <form action="{{ route('posts.store') }}" method="post">
+                        @csrf
+                        <label class="text-left">口コミ投稿<br>
+                        <input type="hidden" name="user_id" value="{{Auth::id()}}">
+                        <input type="hidden" name="plan_id" value="{{$plan->id}}">
+
+                        <p>評価：
+                            非常に良い<input type="radio" name="value" id="value5" value="5">
+                            良い<input type="radio" name="value" id="value4" value="4">
+                            普通<input type="radio" name="value" id="value3" value="3">
+                            悪い<input type="radio" name="value" id="value2" value="2">
+                            非常に悪い<input type="radio" name="value" id="value1" value="1">
+                        </p>
+                        <p>タイトル：　<input type="text" name="title"></p>
+                        <p>ペンネーム：　<input type="name" name="poster_name"></p>
+                        感想：　<input type="text" name="content"></label>
+                        <button type="submit">投稿</button>
+                    </form>
+                </div>
                 
             </div>
         </div>
+        @endforeach
 @endsection
