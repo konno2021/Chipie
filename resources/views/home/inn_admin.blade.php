b3ce6f4d75121347b35ad9b2078911bec500afd8
Taneishi-minori <m.taneisi2021php@gmail.com>
2021-05-25 15:47:49 +0900

    taneisihi

--- resources/views/home/inn_admin.blade.php
+++ resources/views/home/inn_admin.blade.php
@@ -1,36 +1,45 @@
 {{-- 宿アカウントのトップページ(プラン一覧) --}}
 @extends('commons.template')
 
-@section('content')
-    @foreach($plans as $plan)
-        <div class="card ml-3 mr-3">
-            <div class="card-body">
-                <div class="card-text">
-                <p>プラン名：{{ $plan }}</p>
-                    <table class="table">
-                        <tr>
-                            <th class="table-active pt-1 pb-1 text-center" style="width:150px"></th>
-                            <td class="table-light pt-1 pb-1">{{ $inn->address }}</td>
-                        </tr>
-                        <tr>
-                            <th class="table-active pt-1 pb-1 text-center">電話番号</th>
-                            <td class="table-light pt-1 pb-1">{{ $inn->tel }}</td>
-                        </tr>
-                        <tr>
-                            <th class="table-active pt-1 pb-1 text-center">メールアドレス</th>
-                            <td class="table-light pt-1 pb-1">{{ $inn->email }}</td>
-                        </tr>
-                        <tr>
-                            <th class="table-active pt-1 pb-1 text-center">チェックイン</th>
-                            <td class="table-light pt-1 pb-1">{{ date('H:i', strtotime($inn->check_in)) }}</td>
-                        </tr>
-                        <tr>
-                            <th class="table-active pt-1 pb-1 text-center">チェックアウト</th>
-                            <td class="table-light pt-1 pb-1">{{ date('H:i', strtotime($inn->check_out)) }}</td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-        </div>
-    @endforeach
+    @section('content')
+        <h2 class="mb-3 pt-3">◇プラン一覧</h2>
+
+    <table class="table table-bordered">
+        <thead  class="thead-dark">
+            <tr class="pd-2">
+                <th>プランID</th>
+                <th>プラン名</th>
+                <th>価格</th>
+                <th>プラン説明</th>
+                <th>部屋数</th>
+                <th>プラン開始時期</th>
+                <th>プラン終了時期</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($plans as $plan)
+                <tr v-for="(user,index) in users" v-bind:key="index">
+                    <td>{{$plan->id}}</td>
+                    <td>{{$plan->name}}</td>
+                    <td>{{$plan->price}}</td>
+                    <td>{{$plan->description}}</td>
+                    <td>{{$plan->room}}</td>
+                    <td>{{$plan->started_at}}</td>
+                     <td>{{$plan->ended_at}}</td>
+                    <td>
+                        <div class="form-row">
+                            <button><a href="{{route('plans.edit', $plan)}}">変更</a></button>
+                            <form method="post" action="{{route('plans.destroy', $plan)}}">
+                                @csrf
+                                @method('delete')
+                                    <button class="btn btn-danger">削除
+                                                
+                                    </button>
+                            </form>
+                            </div>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 @endsection
