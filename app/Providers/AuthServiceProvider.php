ed106651bbce9a8726d69bbc6511f7071b2d37b4
kageyamasan2 <2021php41-student013@la-study.com>
2021-05-27 19:36:00 +0900

    zangyou kage

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,6 +14,9 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
+        'App\User' => 'App\Policies\UserPolicy',
+        'App\Plan' => 'App\Policies\PlanPolicy',
+
     ];
 
     /**
